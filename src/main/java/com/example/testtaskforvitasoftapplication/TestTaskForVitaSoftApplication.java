bda19d771624e7f8aa5baea677681f467c35f4f7
ValeryyEmelyanov <130102631+ValeryyEmelyanov@users.noreply.github.com>
2024-10-21 02:35:16 +0300

    Создан проект на Java 11 с использованием Spring Boot версии 2.7.14.

---
+++ src/main/java/com/example/testtaskforvitasoftapplication/TestTaskForVitaSoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtaskforvitasoftapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskForVitaSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskForVitaSoftApplication.class, args);
+    }
+
+}
